74ebcf8c6374f9a60733963040cfc6eafe1a3fce
Rafael Cesar Ferreira <rafacesar0070@gmail.com>
2022-10-12 12:22:27 -0300

    four commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    
+    <div>
+
+      <h1 className="text-3xl font-bold underline"> Hello World </h1>
+
+    </div>
+
+  )
 }
 
 export default HomePage
